c84707f60edabf5199b7d90ebceff9b84425eee9
WillChew <williamchew95@gmail.com>
2018-06-04 14:00:44 -0400

    initial commit

--- String/main.m
+++ String/main.m
@@ -9,9 +9,39 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+   
+    //NSString/NSMutableString
+    
+    NSString *s1 = @"Hello";
+    NSString *s2 = @" Lighthouse";
+    NSString *s3 = [s1 stringByAppendingString:s2];
+
+    NSString *s4 = [NSString stringWithFormat:@"line %d: Today is June %d, %@%@", __LINE__, 4, s1,s2];
+    NSLog(@"%@", s4);
+    if (s1 == s2) {
+        
     }
+    
+    if ([s1 isEqualToString:s2]) {} else {
+        NSLog(@"They are not equal");
+    }
+    
+    NSMutableString *ms1 = [NSMutableString stringWithString:@"my mutable string"];
+    NSString *s5 = ms1;
+    
+    [ms1 appendString:@" WOW"];
+      
+      NSRange range = NSMakeRange(5, 2);
+      NSString *s6 = [s4 substringWithRange: range];
+      
+      NSString *s7 = [ms1 copy];
+      NSMutableString *ms2 = [s6 mutableCopy];
+      
+//      NSMutableString *ms3 = (NSMutableString*)s5;
+      
+      ms2 = nil;
+    
+    
+    
     return 0;
 }
